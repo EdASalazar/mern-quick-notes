805e2cc6032be4f8cb0772be059c9baf36f4f48f
EdASalazar <edasalazar123@gmail.com>
2023-03-21 22:47:01 -0700

    Add Note field

--- src/pages/NewNotePage/NewNotePage.jsx
+++ src/pages/NewNotePage/NewNotePage.jsx
@@ -2,7 +2,10 @@ export default function NewNotePage() {
   return (
     <div className="NewNotePage">
       <h2>Add A New Note</h2>
-      
+      <form action="">
+        <input type="text" name="text" placeholder="New Note..."/>
+        <button type="submit">Add A Note</button>
+      </form>
 
 
     </div>
