acf8d2c78dddc8de0c242f42159d99fe4f365f5a
EdASalazar <edasalazar123@gmail.com>
2023-03-23 09:46:00 -0700

    trying to format the date

--- src/pages/NoteItemPage/NoteItemPage.jsx
+++ src/pages/NoteItemPage/NoteItemPage.jsx
@@ -4,7 +4,7 @@ export default function NoteItemPage({date, text}){
   return (
     <div className="NoteItemPage">
         <li>
-          {text}&nbsp; | &nbsp;Added:{date}
+          {text}&nbsp; | &nbsp;Added:{date.toLocaleString("en-US")}
         </li>
     </div>
   )
