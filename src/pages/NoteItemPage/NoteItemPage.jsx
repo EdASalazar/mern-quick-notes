e77d6caac4a7ce10998563f9030d948b875550f8
EdASalazar <edasalazar123@gmail.com>
2023-03-23 09:16:18 -0700

    formatting

--- src/pages/NoteItemPage/NoteItemPage.jsx
+++ src/pages/NoteItemPage/NoteItemPage.jsx
@@ -4,7 +4,7 @@ export default function NoteItemPage({date, text}){
   return (
     <div className="NoteItemPage">
         <li>
-          {text} |  Added: {date.toLocaleString()} 
+          {text}&nbsp; | &nbsp;Added:{date}
         </li>
     </div>
   )
